8d55420b1e087880d36839448f06b05caf8d807a
Racket Package Dependency Fixer <jackhfirth+racket-dep-fixer@gmail.com>
2015-10-06 02:02:30 +0000

    Fix dependencies.

--- info.rkt
+++ info.rkt
@@ -2,11 +2,14 @@
 (define collection "identikon")
 (define version "1.0.1")
 (define scribblings '(("scribblings/identikon.scrbl" ())))
-(define deps '("base"
+(define deps '("draw-lib"
+               "gui-lib"
+               "base"
                "sugar"
                "css-tools"
                "htdp-lib"
                "quickcheck"))
-(define build-deps '("racket-doc"
+(define build-deps '("rackunit-lib"
+                     "racket-doc"
                      "scribble-lib"))
 (define raco-commands '(("identikon" (submod identikon main) "issue Identikon command" #f)))
